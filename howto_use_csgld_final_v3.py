209719c0cdeadc58e5c371b8985d04aaa4dc781d
Wei Deng <greyman@live.cn>
2022-12-20 12:14:46 -0500

    add numerically stable mixture density

--- howto_use_csgld_final_v3.py
+++ howto_use_csgld_final_v3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import jax
+import jax.scipy as jsp
 import jax.numpy as jnp
+
 import blackjax
 import blackjax.sgmcmc.gradients as gradients
 
@@ -38,17 +40,10 @@ init_position = 10.
 def logprior_fn(position):
     return 0
 
-'''
 def loglikelihood_fn(position, x):
-    mixture_1 = jax.scipy.stats.norm.pdf(x, loc=position, scale=sigma)
-    mixture_2 = jax.scipy.stats.norm.pdf(x, loc=-position + gamma, scale=sigma)
-    return jnp.log(0.5 * mixture_1 + 0.5 * mixture_2).sum()
-'''
-
-def loglikelihood_fn(position, x): # stable implementation of Gaussian mixture
-    energy_1 = -(x-position)**2 / 2 / sigma**2
-    energy_2 = -(x+position-gamma)**2 / 2 / sigma**2
-    return jax.scipy.special.logsumexp(jnp.array([energy_1, energy_2]))
+    loglikelihood_fn_1 = jax.scipy.stats.multivariate_normal.logpdf(x, position, sigma**2)
+    loglikelihood_fn_2 = jax.scipy.stats.multivariate_normal.logpdf(x, -position + gamma, sigma**2)
+    return jsp.special.logsumexp(jnp.array([loglikelihood_fn_1, loglikelihood_fn_2])) + jnp.log(0.5)
 
 ### compute the energy function
 def energy_fn(position, minibatch):
