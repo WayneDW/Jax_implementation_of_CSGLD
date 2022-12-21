9942829cc7e644bda3d5dad6fbb76a860c74500a
Wei Deng <greyman@live.cn>
2022-12-21 11:54:25 -0500

    .

--- howto_use_csgld_final_v5.py
+++ howto_use_csgld_final_v5.py
@@ -209,7 +209,7 @@ plt.close()
 
 # 3.3 Analyze why CSGLD works
 smooth_energy_pdf = jnp.convolve(state.energy_pdf, jsp.stats.norm.pdf(jnp.arange(-100, 101), scale=50), mode='same')
-interested_idx = jax.lax.floor((jnp.arange(3700, 10000)) / energy_gap).astype('int32')
+interested_idx = jax.lax.floor((jnp.arange(3750, 10000)) / energy_gap).astype('int32')
 plt.plot(jnp.arange(num_partitions)[interested_idx]*energy_gap, smooth_energy_pdf[interested_idx])
 plt.xlabel(f'Energy')
 plt.ylabel('Density')
