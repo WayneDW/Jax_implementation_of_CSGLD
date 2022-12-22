87655d5c978172252384f9df7abd2b8b38c0713d
Wei Deng <greyman@live.cn>
2022-12-21 22:03:46 -0500

    .

--- howto_use_csgld_2d_example_compare_with_csgld.py
+++ howto_use_csgld_2d_example_compare_with_csgld.py
@@ -1,8 +1,44 @@
+#!/usr/bin/env python3
+
 import itertools
 
 import jax
 import jax.scipy as jsp
 import jax.numpy as jnp
+
+from typing import Callable, NamedTuple
+from blackjax.types import Array, PRNGKey, PyTree
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+''' starting module for hyperparameter tuning '''
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Grid search')
+parser.add_argument('-lr',  default=1e-3, type=float)
+parser.add_argument('-zeta',  default=2, type=float)
+parser.add_argument('-sz',  default=10, type=float)
+parser.add_argument('-temperature',  default=1, type=float)
+parser.add_argument('-num_partitions',  default=10000, type=int)
+parser.add_argument('-energy_gap',  default=0.001, type=float)
+
+pars = parser.parse_args()
+
+
+
+lr = pars.lr
+zeta = pars.zeta
+sz = pars.sz
+temperature = pars.temperature
+
+### The following parameters partition the energy space and no tuning is needed. 
+num_partitions = pars.num_partitions
+energy_gap = pars.energy_gap
+''' ending module for hyperparameter tuning '''
+
 
 
 lmbda = 1/25
@@ -24,9 +60,6 @@ def sample_fn(rng_key):
 
 ## SGLD baseline 
 
-import jax
-from jax import numpy as jnp
-
 import blackjax
 from fastprogress import progress_bar
 
@@ -43,22 +76,15 @@ rng_key = jax.random.PRNGKey(3)
 init_position = -10 + 20 * jax.random.uniform(rng_key, shape=(2,))
 
 position = init_position
+
+'''
 sgld_samples = []
 for i in progress_bar(range(num_training_steps)):
     _, rng_key = jax.random.split(rng_key)
     position = jax.jit(sgld)(rng_key, position, 0, schedule[i])
     sgld_samples.append(position)
 
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 sgld_samples = jnp.array(sgld_samples)
-
-#sns.kdeplot(x=sgld_samples[:, 0], y=sgld_samples[:, 1],  cmap="Blues", fill=True, thresh=0.05, bw_method=0.15)
-
-#plt.savefig("./2d_sgld.pdf")
-#plt.close()
 
 fig, scatter = plt.subplots(figsize = (20, 20), dpi = 100)
 kde = sns.kdeplot(x=sgld_samples[:, 0], y=sgld_samples[:, 1],  cmap="Blues", fill=True, thresh=0.05, bw_method=0.15)
@@ -66,3 +92,71 @@ kde.set_xlim(left=-5, right=5)
 kde.set_ylim(bottom=-5, top=5)
 plt.savefig("./2d_sgld.pdf")
 plt.close()
+'''
+
+### CSGLD part
+
+
+class CSGLDState(NamedTuple):
+    position: PyTree
+    energy_pdf: Array
+    energy_idx: int
+
+'''
+### specify hyperparameters (zeta and sz are the only two hyperparameters to tune)
+lr = 1e-3
+zeta = 2
+sz = 10
+temperature = 1
+
+### The following parameters partition the energy space and no tuning is needed. 
+num_partitions = 10000
+energy_gap = 0.001
+'''
+min_energy = -2
+
+
+thinning_factor = 100
+
+csgld = blackjax.csgld(
+    logprob_fn,
+    grad_fn,
+    zeta=zeta,  # can be specified at each step in lower-level interface
+    temperature=temperature,  # can be specified at each step
+    num_partitions=num_partitions,  # cannot be specified at each step
+    energy_gap=energy_gap,  # cannot be specified at each step
+    min_energy=min_energy,
+)
+
+## 3.1 Simulate via the CSGLD algorithm
+state = csgld.init(init_position)
+
+csgld_samples = [] 
+#csgld_sample_list, csgld_energy_idx_list = jnp.array([]), jnp.array([])
+for iter_ in progress_bar(range(num_training_steps)):
+    rng_key, subkey = jax.random.split(rng_key)
+    stepsize_SA = min(1e-2, (iter_+100)**(-0.8)) * sz
+    state = jax.jit(csgld.step)(subkey, state, 0, lr, stepsize_SA)
+    csgld_samples.append(state.position)
+
+csgld_samples = jnp.array(csgld_samples)
+
+fig, scatter = plt.subplots(figsize = (20, 20), dpi = 100)
+kde = sns.kdeplot(x=csgld_samples[:, 0], y=csgld_samples[:, 1],  cmap="Blues", fill=True, thresh=0.05, bw_method=0.15)
+kde.set_xlim(left=-5, right=5)
+kde.set_ylim(bottom=-5, top=5)
+plt.savefig(f"./2d_csgld_before_resampling_lr_{lr}_zeta_{zeta}_sz_{sz}_T_{temperature}_Egap_{energy_gap}_num_part_{num_partitions}.pdf")
+plt.close()
+
+'''
+energy_pdf = state.energy_pdf
+
+csgld_energy_idx_list = jnp.array([])
+# get index of each particle for importance sampling
+for position in progress_bar(csgld_samples):
+    energy_value = logprob_fn(position))
+    idx = jax.lax.min(jax.lax.max(jax.lax.floor((energy_value - min_energy) / energy_gap + 1).astype("int32"), 1,), num_partitions - 1, )
+    csgld_energy_idx_list = jnp.append(csgld_energy_idx_list, idx)
+'''
+
+
